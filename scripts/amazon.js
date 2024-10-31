b01d7aa932154476d02382a3bee55ee025652bb5
Eric Nwogu <reeko3d@gmail.com>
2024-10-31 14:45:28 +0000

    exercises

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,8 @@
-import { cart, addToCart } from "../data/cart.js";
+import { cart, addToCart, calculateCartQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
+
+updateCartQuantity();
 
 
 let productsHTML = '';
@@ -62,14 +64,17 @@ products.forEach((product) => {
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 function updateCartQuantity() {
-  let cartQuantity = 0;
+  const cartQuantity = calculateCartQuantity();
 
-    cart.forEach((cartItem) => {
-      cartQuantity += cartItem.quantity
-    })
-
-    document.querySelector('.js-cart-quantity')
+    if (cartQuantity !== 0) {
+      document.querySelector('.js-cart-quantity')
     .innerHTML = cartQuantity
+    } else {
+      document.querySelector('.js-cart-quantity')
+    .innerHTML = ''
+    }
+
+    
 }
 
 
