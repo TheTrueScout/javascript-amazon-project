59c29d9ab8d87dd60810c5503a15a77b44d21527
Eric Nwogu <reeko3d@gmail.com>
2024-11-14 17:45:36 +0000

    updated page content to match tutorial

--- data/cart.js
+++ data/cart.js
@@ -1,19 +1,16 @@
 export let cart = JSON.parse(localStorage.getItem('cart'));
 
 if (!cart) {
-  cart = [
-    {
-      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      quantity: 2
-    },
-    {
-      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-      quantity: 1
-    }
-  ];
-};
-
-
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+    deliveryOptionId: '1'
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1,
+    deliveryOptionId: '2'
+  }];
+}
 
 function saveToStorage() {
   localStorage.setItem('cart', JSON.stringify(cart));
@@ -26,48 +23,36 @@ export function addToCart(productId) {
     if (productId === cartItem.productId) {
       matchingItem = cartItem;
     }
-  })
+  });
 
   if (matchingItem) {
-    matchingItem.quantity += 1
+    matchingItem.quantity += 1;
   } else {
     cart.push({
       productId: productId,
-      quantity: 1
-    })
+      quantity: 1,
+      deliveryOptionId: '1'
+    });
   }
 
-  saveToStorage()
+  saveToStorage();
 }
 
-
 export function removeFromCart(productId) {
-  const newCart = []
+  const newCart = [];
 
   cart.forEach((cartItem) => {
     if (cartItem.productId !== productId) {
-      newCart.push(cartItem)
+      newCart.push(cartItem);
     }
   });
 
   cart = newCart;
-  
+
   saveToStorage();
 }
 
-export function calculateCartQuantity() {
-  
-  let cartQuantity = 0
-
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
-
-  return cartQuantity;
-}
-
-
-export function updateQuantity(productId, newQuantity) {
+export function updateDeliveryOption(productId, deliveryOptionId) {
   let matchingItem;
 
   cart.forEach((cartItem) => {
@@ -76,7 +61,7 @@ export function updateQuantity(productId, newQuantity) {
     }
   });
 
-  matchingItem.quantity = newQuantity;
+  matchingItem.deliveryOptionId = deliveryOptionId;
 
   saveToStorage();
 }
